f064e49434d492a0d491adafdd6da24deeaaad44
Alexander Martinz <eviscerationls@gmail.com>
2016-07-03 19:07:32 +0200

    Remove Doze package
    -----
    
    Revert "oneplus2: doze improvements"
    
    This reverts commit 67ff005534764ee83443d8dd0454b674d965e08b.
    
    Revert "oneplus2: add doze package"
    
    This reverts commit 3195672fcd02694a7a31b1c85a568e51ac1e9c21.
    
    -----
    
    Change-Id: Ica95b746f3320dded2e43bb3994f49602766e5b6
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- device.mk
+++ device.mk
@@ -127,10 +127,6 @@ PRODUCT_PACKAGES += \
     liboverlay \
     libtinyxml
 
-# Doze mode
-PRODUCT_PACKAGES += \
-    OneplusDoze
-
 # Filesystem management tools
 PRODUCT_PACKAGES += \
     e2fsck \
